f0c1d7fe2f9f76c9ea09f14232376f29ff182de7
CODZR <975336710@qq.com>
2024-02-07 21:25:13 +0800

    init project

--- dbt_dagster_poetry/dagster_tutorial/__init__.py
+++ dbt_dagster_poetry/dagster_tutorial/__init__.py
@@ -1,14 +1,13 @@
 ## Refer to Using dbt with Dagster, part two for info about this file:
 ## https://docs.dagster.io/integrations/dbt/using-dbt-with-dagster/part-two
 
-# /tutorial_template/tutorial_dbt_dagster/__init__.py
+# /tutorial_template/dbt_jaffle_shop/__init__.py
 
 import os
 
 from dagster_dbt import DbtCliClientResource
-from tutorial_dbt_dagster import assets
-from dagster_duckdb_pandas import duckdb_pandas_io_manager
-from tutorial_dbt_dagster.assets import DBT_PROFILES, DBT_PROJECT_PATH
+from dagster_tutorial import assets
+from dagster_tutorial.assets import DBT_PROFILES, DBT_PROJECT_PATH
 
 from dagster_tutorial import Definitions, load_assets_from_modules
 
