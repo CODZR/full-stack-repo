1b64a458e4d743fc43b692a3f56a9c75fdd0388f
CODZR <975336710@qq.com>
2024-02-09 11:48:48 +0800

    init project

--- dagster_project/__init__.py
+++ dagster_project/__init__.py
@@ -1,56 +1,30 @@
 import os
 
-from dagster import (
-    Definitions,
-    FilesystemIOManager,
-    ScheduleDefinition,
-    define_asset_job,
-    load_assets_from_package_module,
-)
+from dagster_dbt import dbt_cli_resource, dbt_test_op, load_assets_from_dbt_project
 
-from .assets import forecasting, raw_data
-from .assets.dbt import (
-    DBT_PROJECT_DIR,
-    dbt_project_assets,
-    dbt_resource,
-)
+from dagster import Definitions, file_relative_path
+from dagster import job
 
-raw_data_assets = load_assets_from_package_module(
-    raw_data,
-    group_name="raw_data",
-    # all of these assets live in the duckdb database, under the schema raw_data
-    key_prefix=["duckdb", "raw_data"],
-)
+DBT_PROJECT_PATH = file_relative_path(__file__, "../dbt")
+DBT_PROFILES = file_relative_path(__file__, "../dbt/config")
 
-forecasting_assets = load_assets_from_package_module(
-    forecasting,
-    group_name="forecasting",
-)
 
-# define jobs as selections over the larger graph
-everything_job = define_asset_job("everything_everywhere_job", selection="*")
-forecast_job = define_asset_job(
-    "refresh_forecast_model_job", selection="*order_forecast_model"
-)
+dbt_resource = dbt_cli_resource.configured(
+        {
+            "project_dir": DBT_PROJECT_PATH,
+            "profiles_dir": DBT_PROFILES,
+        }
+    )
 
-io_manager = FilesystemIOManager(
-    base_dir="data"  # 相对路径，在 dagster dev 运行的文件夹下
-)
-
-resources = {
-    # this io_manager allows us to load dbt models as pandas dataframes
-    "io_manager": io_manager,
-    # this io_manager is responsible for storing/loading our pickled machine learning model
-    "model_io_manager": FilesystemIOManager(),
-    # this resource is used to execute dbt cli commands
-    "dbt": dbt_resource,
+model_resources = {
+    "dbt": dbt_resource
 }
 
-defs = Definitions(
-    assets=[dbt_project_assets, *raw_data_assets, *forecasting_assets],
-    resources=resources,
-    schedules=[
-        ScheduleDefinition(job=everything_job, cron_schedule="@weekly"),
-        ScheduleDefinition(job=forecast_job, cron_schedule="@daily"),
-    ],
-)
+
+
+@job(resource_defs={'dbt': dbt_resource})
+def run_dbt_test_job():
+    dbt_test_op()
+
+defs = Definitions(assets=load_assets_from_dbt_project(DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, use_build_command=True), resources=model_resources, jobs=[run_dbt_test_job])
+
