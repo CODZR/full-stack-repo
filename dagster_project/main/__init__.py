3ac8b5fe2cb0065a062c44583e8d096b1d0a36a9
CODZR <975336710@qq.com>
2024-02-23 11:04:02 +0800

    init project

---
+++ dagster_project/main/__init__.py
@@ -0,0 +1,17 @@
+from dagster import Definitions, load_assets_from_modules, FilesystemIOManager
+from dagster_dbt import DbtCliClientResource
+
+from . import assets
+from .assets import DBT_PROFILES, DBT_PROJECT_PATH
+
+resources = {
+    "dbt": DbtCliClientResource(
+        project_dir=DBT_PROJECT_PATH,
+        profiles_dir=DBT_PROFILES,
+    ),
+    "io_manager": FilesystemIOManager(
+        base_dir="data"  # 相对路径，在 dagster dev 运行的文件夹下
+    ),
+}
+
+defs = Definitions(assets=load_assets_from_modules([assets]), resources=resources)
--- dbt_project/models/staging/kangxi/stg_hanzis.sql
+++ dbt_project/models/staging/kangxi/stg_hanzis.sql
@@ -7,6 +7,30 @@
 SELECT
   id AS hanzi_id,
   zi,
-  pinyin
+  pinyin,
+  bushou,
+  bushoubh,
+  zbh,
+  kxzdbh,
+  wb86,
+  wb98,
+  UNICODE,
+  hzwx,
+  jxyy,
+  cyz,
+  xmx,
+  bsdx,
+  jbjs,
+  xhzdxxjs,
+  hydzdjs,
+  kxzdjs,
+  swjzxj,
+  swjzxjpic,
+  zyybpic,
+  xgsf,
+  xgcy,
+  xgchengyu,
+  xgsc,
+  kxzdpic
 FROM
   {{ ref('raw_hanzis') }}
